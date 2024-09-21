1d3930124833c698913abc862dd3bb70f3c74bc7
ProjectAhkdan <ahkdanprika1@gmail.com>
2024-09-22 04:54:44 +0700

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,106 @@
+import { BackgroundGradientAnimation } from "@/components/ui/bg-gradient";
 import Image from "next/image";
+import icon from "../../public/icon/verified.png";
+import instagramIcon from "../../public/icon/instagram.png";
+import whatsappIcon from "../../public/icon/whatsapp.png"; 
+import arrowIcon from "../../public/icon/arrow-right.png"; 
+
+const Datasosmed = [
+  {
+    id: 1,
+    name: "Instagram",
+    url: "https://www.instagram.com/abassstravel/",
+    icon: instagramIcon,
+  },
+  {
+    id: 2,
+    name: "WhatsApp",
+    url:
+      "https://api.whatsapp.com/send?phone=6281312048683&text=" +
+      encodeURIComponent(
+        "Halo Abasstravel! Saya sangat tertarik dengan layanan perjalanan Anda. Apakah ada paket khusus yang dapat Anda tawarkan? Saya berharap dapat mendengar lebih banyak tentang keindahan yang ditawarkan!"
+      ),
+    icon: whatsappIcon,
+  },
+];
+
+const buttons = [
+  { id: 1, name: "Button 1", icon: instagramIcon, url: "#" },
+  { id: 2, name: "Button 2", icon: whatsappIcon, url: "#" },
+  { id: 3, name: "Button 3", icon: instagramIcon, url: "#" },
+  { id: 4, name: "Button 4", icon: whatsappIcon, url: "#" },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="w-full h-screen relative">
+      <div className="w-full h-72 relative overflow-hidden">
+        <BackgroundGradientAnimation />
+      </div>
+      <div className="w-full h-full bg-black/85 bg-cover relative">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/noise.png"
+          width={400}
+          height={400}
+          alt="noise"
+          objectFit="cover"
+          className="w-full h-full opacity-60"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </div>
+      <div className="absolute inset-0 m-auto h-full flex flex-col top-96 items-center"> 
+        <div className="w-44 h-44 bg-white rounded-full flex items-center justify-center"></div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="mt-4 text-white text-lg w-full text-center px-4">
+          <div className="flex gap-2 items-center justify-center">
+            <p className="font-semibold text-[24px] sm:text-[20px]">
+              Travel with Abassstravel
+            </p>
+            <div>
+              <Image src={icon} alt="icon" width={32} height={32} />
+            </div>
+          </div>
+          <div className="mt-2">
+            <a href="#" className="text-blue-600 text-sm sm:text-base">
+              https://example.com
+            </a>
+          </div>
+          <p className="font-normal text-sm">
+            Keindahan alam yang bisa ditemukan di setiap sudut dunia
+          </p>
+
+          <div className="mt-4 gap-3 flex justify-center">
+            {Datasosmed.map((sosmed) => (
+              <a
+                key={sosmed.id}
+                href={sosmed.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center text-black text-xs font-semibold bg-white rounded-lg p-2 m-2"
+              >
+                <Image src={sosmed.icon} alt={sosmed.name} width={20} height={20} className="mr-2" />
+                {sosmed.name}
+              </a>
+            ))}
+          </div>
+
+
+          <div className="w-full mt-8 px-[26rem] max-lg:px-60 max-sm:px-5 max-md:px-0">
+            {buttons.map((button) => (
+              <a
+                key={button.id}
+                href={button.url}
+                className="flex items-center justify-between w-full bg-white rounded-lg p-3 mb-2 hover:bg-gray-200"
+              >
+                <div className="flex items-center">
+                  <Image src={button.icon} alt={button.name} width={20} height={20} className="mr-2" />
+                  <span className="text-black font-semibold">{button.name}</span>
+                </div>
+                <Image src={arrowIcon} alt="arrow" width={16} height={16} />
+              </a>
+            ))}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
